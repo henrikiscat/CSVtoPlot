39b4b930448a4ae5acbc41f3487cc31c063c5591
Henrik Isaksson <henrik@debian>
2021-05-12 12:17:41 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='CSVtoPlot',
+    version='0.0.1b',
+    packages=[''],
+    url='https://github.com/henrikiscat/CSVtoPlot',
+    license='GPLv3',
+    author='Henrik Isaksson',
+    author_email='henrikis@protonmail.com',
+    description='PySimpleGUI script to plot CSV measurement data from PEC equipment.'
+)
